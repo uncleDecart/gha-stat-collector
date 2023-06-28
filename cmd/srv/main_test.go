39966cb6aba5ef39da02ef77012d2089eadf55a0
Pavel Abramov <uncle.decart@gmail.com>
2023-06-28 18:11:54 +0200

    Add publish handle test
    Signed-off-by: Pavel Abramov <uncle.decart@gmail.com>

---
+++ cmd/srv/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/uncleDecart/gha-stat-collector/controllers"
+	"gotest.tools/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	type PingResponse struct {
+		Message string `json:"message"`
+	}
+	var got PingResponse
+	json.NewDecoder(w.Body).Decode(&got)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, PingResponse{Message: "pong"}, got)
+}
+
+func TestPublishRoute(t *testing.T) {
+	at := "qwerty"
+	os.Setenv("ACCESS_TOKEN", at)
+
+	router := setupRouter()
+
+	body := controllers.ActionLogEntry{
+		Name:        "test-action",
+		Start:       "2023-02-01",
+		End:         "2023-02-05",
+		ExecTime:    "42",
+		Successfull: true,
+	}
+
+	b, _ := json.Marshal(body)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/publish", bytes.NewReader(b))
+	req.Header.Set("auth", at)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+}

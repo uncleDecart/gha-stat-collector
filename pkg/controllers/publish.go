8a02622aacd1733976364005ff34df36095e149e
Pavel Abramov <uncle.decart@gmail.com>
2023-07-24 11:50:03 +0200

    Move `publish.go` types to separate file
    This is needed because other controllers might need
    same types, for instance to read same data
    
    Signed-off-by: Pavel Abramov <uncle.decart@gmail.com>

--- pkg/controllers/publish.go
+++ pkg/controllers/publish.go
@@ -9,23 +9,6 @@ import (
 	"github.com/uncleDecart/gha-stat-collector/pkg/models"
 )
 
-type StepLogEntry struct {
-	Id       uint64 `json:"id" binding:"required"`
-	ExecTime string `json:"exec_time" binding:"required"`
-	// why *bool: see https://github.com/gin-gonic/gin/issues/814
-	Successfull *bool `json:"successful" binding:"required"`
-}
-
-type ActionLogEntry struct {
-	Name  string `json:"name" binding:"required"`
-	Start string `json:"start" binding:"required"`
-	End   string `json:"end" binding:"required"`
-	// why *bool: see https://github.com/gin-gonic/gin/issues/814
-	Successfull *bool          `json:"successful" binding:"required"`
-	Arch        string         `json:"arch" binding:"required"`
-	Steps       []StepLogEntry `json:"steps"`
-}
-
 func Publish(c *gin.Context) {
 	var entry ActionLogEntry
 
---
+++ pkg/controllers/types.go
@@ -0,0 +1,18 @@
+package controllers
+
+type StepLogEntry struct {
+	Id       uint64 `json:"id" binding:"required"`
+	ExecTime string `json:"exec_time" binding:"required"`
+	// why *bool: see https://github.com/gin-gonic/gin/issues/814
+	Successfull *bool `json:"successful" binding:"required"`
+}
+
+type ActionLogEntry struct {
+	Name  string `json:"name" binding:"required"`
+	Start string `json:"start" binding:"required"`
+	End   string `json:"end" binding:"required"`
+	// why *bool: see https://github.com/gin-gonic/gin/issues/814
+	Successfull *bool          `json:"successful" binding:"required"`
+	Arch        string         `json:"arch" binding:"required"`
+	Steps       []StepLogEntry `json:"steps"`
+}

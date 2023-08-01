b4474f46e63330bff6351a58963113d22f88577e
Pavel Abramov <uncle.decart@gmail.com>
2023-08-01 11:09:01 +0200

    Add comparison functions for timing types
    Since Gin boolean values which are required has to be pointers this
    makes simple comparison between two objects behave the way we don't
    want. That is why we need to introduce comparison operators
    
    Signed-off-by: Pavel Abramov <uncle.decart@gmail.com>

--- pkg/controllers/types.go
+++ pkg/controllers/types.go
@@ -26,3 +26,59 @@ type ActionLogEntrySearch struct {
 	CurPage    int64            `json:"curPage"`
 	Data       []ActionLogEntry `json:"data"`
 }
+
+func (lhs StepLogEntry) Compare(rhs StepLogEntry) bool {
+	return lhs.Id == rhs.Id &&
+		lhs.ExecTime == rhs.ExecTime &&
+		*lhs.Successfull == *rhs.Successfull
+}
+
+func CompareStepLogEntrySlice(lhs, rhs []StepLogEntry) bool {
+	lhsLen := len(lhs)
+	if lhsLen != len(rhs) {
+		return false
+	}
+	for i := 0; i < lhsLen; i++ {
+		if !lhs[i].Compare(rhs[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func (lhs ActionLogEntry) Compare(rhs ActionLogEntry) bool {
+	if lhs.Name != rhs.Name {
+		return false
+	}
+	if lhs.Start != rhs.Start {
+		return false
+	}
+	if lhs.End != rhs.End {
+		return false
+	}
+	if *lhs.Successfull != *rhs.Successfull {
+		return false
+	}
+	if lhs.Arch != rhs.Arch {
+		return false
+	}
+	if !CompareStepLogEntrySlice(lhs.Steps, rhs.Steps) {
+		return false
+	}
+	return true
+}
+
+func CompareActionLogEntrySlice(lhs, rhs []ActionLogEntry) bool {
+	lenLhs := len(lhs)
+	if lenLhs != len(rhs) {
+		return false
+	}
+
+	for i := 0; i < lenLhs; i++ {
+		if !lhs[i].Compare(rhs[i]) {
+			return false
+		}
+	}
+
+	return true
+}
